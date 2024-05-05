cc085816906c4fed0922a1ba2c4ee4a37ef80979
deeksha <deekshajindal058@gmail.com>
2024-05-05 09:23:04 +0530

    updated Google Oauth

--- bookHaul/src/main/java/com/BookHaul/bookHaul/config/SpringConfig.java
+++ bookHaul/src/main/java/com/BookHaul/bookHaul/config/SpringConfig.java
@@ -3,15 +3,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SpringConfig {
-
     @Bean
-    protected void configure(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.authorizeRequests()
-                .anyRequest().authenticated()
-                .and().oauth2Login();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return
+                http.authorizeHttpRequests(
+                                auth -> {
+                                    auth.requestMatchers("/").permitAll();
+                                    auth.anyRequest().authenticated();
+                                })
+                        .httpBasic(withDefaults())
+                        .oauth2Login(withDefaults())
+//                        .formLogin(withDefaults())
+                        .build();
     }
 }
